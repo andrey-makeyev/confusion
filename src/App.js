9dec24947fe22f04453db4a86332c7ac0ea85353
andrey-makeyev <andrew.makeyev@gmail.com>
2022-01-29 18:19:51 +0200

    Presentational and Container Components

--- src/App.js
+++ src/App.js
@@ -1,32 +1,17 @@
-import logo from './logo.svg';
-import { Navbar, NavbarBrand } from 'reactstrap';
-import './App.css';
-import Menu from './components/MenuComponent';
-import { DISHES } from './components/shared/dishes'
-import { Component } from 'react';
+import React, { Component } from "react";
+import Main from "./components/MainComponent";
+
+import "./App.css";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      dishes: DISHES
-    };
+
+  render() {
+    return (
+      <div className="App">
+        <Main />
+      </div>
+    );
   }
-  
-  render(){
-  return (
-    <div>
-      <Navbar dark color="primary">
-        <div className='containter'>
-          <NavbarBrand href="/">
-            Ristorante Con Fusion
-          </NavbarBrand>
-        </div>
-      </Navbar>
-      <Menu dishes={this.state.dishes} />
-    </div>
-  );
-}
 }
 
 export default App;
